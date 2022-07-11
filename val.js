e99966429e25cb4e831540e60c0579ab00857fe1
Igor <igor.guchmazovwork@gmail.com>
2022-07-11 15:43:24 +0300

    update

--- val.js
+++ val.js
@@ -1,16 +1,21 @@
 let form = document.querySelector('.formAccount');
 let required = form.querySelector('.required');
+let val = document.querySelector('.input-val');
 let email = form.querySelector('.email');
 let sample = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-$(".number-first-input").mask("+7(999)999-99-99");
-$(".number-second-input").mask("+7(999)999-99-99");
+$(".first-tel").mask("+7(999)999-99-99");
+$(".second-tel").mask("+7(999)999-99-99");
 
-
+// val.classList.add('error');
+// for(let i in required){
+//     console.log(required[i]);
+// }
 form.addEventListener('submit', function(event)
 {
     let error = true;
-    for(let input of required)
+    alert(surname);
+    for(let input of val)
     {
         if(input.value === '')
         {
@@ -19,11 +24,11 @@ form.addEventListener('submit', function(event)
         }
         else input.classList.remove('error');
     }
-    if(sample.test(mail.value) === false)
-    {
-        mail.classList.add('error');
-        error = false;
-    }
+    // if(sample.test(email.value) === false)
+    // {
+    //     email.classList.add('error');
+    //     error = false;
+    // }
     if (error === false) 
     {
         event.preventDefault();
